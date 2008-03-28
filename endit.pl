56c85afcf3cbb8fb9e19dc588b138c667a255763
Mattias Wadenstein <maswan@hpc2n.umu.se>
2008-03-28 14:29:27 +0000

    .

--- endit.pl
+++ endit.pl
@@ -36,7 +36,7 @@ sub readconf($) {
 
 sub printlog($) {
 	my $msg = shift;
-	my $now = strftime '+%Y-%m-%d %H:%M:%S', localtime;
+	my $now = strftime '%Y-%m-%d %H:%M:%S', localtime;
 	open LF, '>>' . $conf{'logdir'} . '/endit.log' or warn "Failed to open " . $conf{'logdir'} . '/endit.log' . ": $!";
 	print LF $now . ': ' . $msg;
 	close LF;
