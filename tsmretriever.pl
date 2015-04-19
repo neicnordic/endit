b2dcb0954932875117f47981d3da7fb0b2cb9618
Lars Viklund <zao@zao.se>
2015-04-20 01:34:55 +0200

    Refactor of retriever for multiple tape-aware dsmc
    Previously, control flow looped with blocking calls to dsmc.
    Now we fork off up to a limit (maxretrievers) calls to dsmc and poll
    them for completion on each iteration of the main loop.
    
    We also prevent jobs from being issued that uses a tape that's recorded
    as being in use by a job.
    
    The code is ready for accepting tape names from request files, but no
    such requests exist yet.

--- tsmretriever.pl
+++ tsmretriever.pl
@@ -4,6 +4,7 @@ use warnings;
 use strict;
 
 use IPC::Run3;
+use POSIX qw( WNOHANG );
 
 use lib '/opt/endit/';
 use Endit qw(%conf readconf printlog getusage);
@@ -39,67 +40,157 @@ sub checkrequest($) {
 		}
 	}
 	if(getpgrp($pid) > 0) {
-		return 1;
+		return { pid => $pid };
 	} else {
 		unlink $req_filename;
-		return 0;
+		return undef;
 	}
 }
-	
+
+sub processing_file($$) {
+	my ($worker,$file) = @_;
+	return exists $worker->{files}->{$file};
+}
+
+my $tapelistmodtime;
+my $tapelist = {};
+my %reqset;
+my @workers;
+
 while(1) {
+#	sleep to let requester remove requests and pace ourselves
 	sleep 60;
-	opendir(REQUEST,$dir . '/request/');
-	my (@requests) = grep { /^[0-9A-Fa-f]+$/ } readdir(REQUEST); # omit entries with extensions
-	closedir(REQUEST);
-	next unless @requests;
-	{
-		open my $lf, ">", $listfile or die "Can't open listfile: $!";
-		foreach my $req (@requests) {
-			if(checkrequest($req)) {
-				print $lf "$dir/out/$req\n";
-			} else {
-				printlog "Deactivating $req due to unexisting pid\n";
+
+#	check if any dsmc workers are done
+	@workers = map {
+		my $w = $_;
+		my $wres = waitpid($w->{pid}, WNOHANG);
+		my $rc = $?;
+		if ($wres == $w->{pid}) {
+#			Child is done
+			$w->{pid} = undef;
+#			TODO(zao): What happens on success/failure?
+			if ($? == 0) {
+			}
+			unlink $w->{listfile};
+		}
+	} @workers;
+	@workers = grep { $_->{pid} } @workers;
+
+#	refresh tape list
+	if (exists $conf{tapefile}) {
+		my $tapefilename = $conf{tapefile};
+		my $newtapemodtime = (stat $tapefilename)[9];
+		if ($newtapemodtime > $tapelistmodtime) {
+			my $newtapelist = Endit::readtapelist($tapefilename);
+			if ($newtapelist) {
+				$tapelist = $newtapelist;
+				$tapelistmodtime = $newtapemodtime;
 			}
 		}
 	}
-	my $indir = $dir . '/in/';
-	my @dsmcopts = split /, /, $conf{'dsmcopts'};
-	my @cmd = ('dsmc','retrieve','-replace=no','-followsymbolic=yes',@dsmcopts, "-filelist=$listfile",$indir);
-	my ($in,$out,$err);
-	$in="A\n";
-	if((run3 \@cmd, \$in, \$out, \$err) && $? ==0) { 
-		# files migrated from tape without issue
-	} else {
-		# something went wrong. figure out what files didn't make it.
-		# wait for the hsm script to remove succesful requests
-		sleep 60;
-		open my $lf, "<", $listfile;
-		my (@requests) = <$lf>;
-		close $lf;
-		my $outfile;
-		my $returncode;
-		foreach $outfile (@requests) {
-			my (@l) = split /\//,$outfile;
-			my $filename = $l[$#l];
-			chomp $filename;
-			my $req = $dir . '/request/' . $filename;
-			if( -e $req ) {
-				my @cmd = ('dsmc','retrieve','-replace=no','-followsymbolic=yes',@dsmcopts,$outfile,$indir);
-				my ($in, $out,$err);
-				$in="A\n";
-				if((run3 \@cmd, \$in, \$out, \$err) && $? ==0) {
-					# Went fine this time. Strange..
-				} else {
-					$returncode=$? >> 8;
-					printlog localtime() . ": warning, dsmc returned $returncode on $outfile:\n";
-					printlog $err;
-					printlog $out;
-					open EF, ">", $req . '.err' or warn "Could not open $req.err file: $!\n";
-					print EF "32\n";
-					close EF;
+
+#	check for new requests
+	{
+		opendir(REQUEST,$dir . '/request/');
+		my (@requests) = grep { /^[0-9A-Fa-f]+$/ } readdir(REQUEST); # omit entries with extensions
+		closedir(REQUEST);
+		if (@requests) {
+			foreach my $req (@requests) {
+				next if (exists $reqset{$req} || grep { processing_file($_, $req) } @workers);
+				my $reqinfo = checkrequest($req);
+				if ($reqinfo) {
+					if (!exists $reqinfo->{tape}) {
+						if (my $tape = $tapelist->{$req}) {
+							$reqinfo->{tape} = $tape;
+						}
+					}
+					$reqset{$req} = $reqinfo;
 				}
 			}
 		}
 	}
-	unlink $listfile;
+
+#	if any requests and free worker
+	if (%reqset && @workers < $conf{'maxretrievers'}) {
+#		make list blacklisting pending tapes
+		my %usedtapes = map { %{$_->{tapes}} } @workers;
+		my %postponed;
+		my $job = {};
+		foreach my $name (keys %reqset) {
+			my $req = $reqset{$name};
+			my $tape;
+			$tape = $req->{tape} if (exists $req->{tape});
+			if (defined $tape && exists $usedtapes{$tape}) {
+				$postponed{$name} = $req;
+			}
+			else {
+				$job->{files}->{$name} = $req;
+				$job->{tapes}->{$tape} = $tape if $tape;
+			}
+		}
+		%reqset = %postponed;
+
+#		start job if non-empty
+		if (exists $job->{files}) {
+			my $listfile = namelistfile();
+			open my $lf, ">", $listfile or die "Can't open listfile: $!";
+			my $files = $job->{files};
+			foreach my $name (keys $files) {
+				print $lf "$dir/out/$name\n";
+			}
+			close $lf;
+
+#			spawn worker
+			my $pid;
+			if ($pid = fork) {
+				$job->{pid} = $pid;
+				push @workers, $job;
+			}
+			else {
+				my $indir = $dir . '/in/';
+				my @dsmcopts = split /, /, $conf{'dsmcopts'};
+				my @cmd = ('dsmc','retrieve','-replace=no','-followsymbolic=yes',@dsmcopts, "-filelist=$listfile",$indir);
+				my ($in,$out,$err);
+				$in="A\n";
+				if((run3 \@cmd, \$in, \$out, \$err) && $? == 0) {
+					# files migrated from tape without issue
+					exit 0;
+				} else {
+					# something went wrong. figure out what files didn't make it.
+					# wait for the hsm script to remove successful requests
+					sleep 60;
+					open my $lf, "<", $listfile;
+					my (@requests) = <$lf>;
+					close $lf;
+					my $outfile;
+					my $returncode = 0;
+					foreach $outfile (@requests) {
+						my (@l) = split /\//,$outfile;
+						my $filename = $l[$#l];
+						chomp $filename;
+						my $req = $dir . '/request/' . $filename;
+						if( -e $req ) {
+							my @cmd = ('dsmc','retrieve','-replace=no','-followsymbolic=yes',@dsmcopts,$outfile,$indir);
+							my ($in, $out,$err);
+							$in="A\n";
+							if((run3 \@cmd, \$in, \$out, \$err) && $? == 0) {
+								# Went fine this time. Strange..
+							} else {
+								$returncode = $? >> 8;
+								printlog localtime() . ": warning, dsmc returned $returncode on $outfile:\n";
+								printlog $err;
+								printlog $out;
+								open EF, ">", $req . '.err' or warn "Could not open $req.err file: $!\n";
+								print EF "32\n";
+								close EF;
+							}
+						}
+					}
+#					Last returncode of the failed single-file runs.
+					exit $returncode;
+				}
+			}
+		}
+	}
 }
