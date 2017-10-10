126134080cb48eaef59aa190f52e4ee51f730ba8
Niklas Edmundsson <nikke@hpc2n.umu.se>
2017-10-10 11:32:46 +0200

    Add proper copyright notices for clarity.

--- HPC2NTSMUtil.pm
+++ HPC2NTSMUtil.pm
@@ -1,8 +1,22 @@
+#   HPC2NTSMUtil - simplify using dsmadmc to interact with TSM server
+#   Copyright (C) 2012-2017 <Niklas.Edmundsson@hpc2n.umu.se>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 package HPC2NTSMUtil;
 
 # vim:ts=4:sw=4:et:
-# $Id: HPC2NTSMUtil.pm,v 1.1 2014/02/24 12:11:10 nikke Exp $
-
 
 use warnings;
 use strict;
