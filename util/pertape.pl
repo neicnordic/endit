43729a4425ffb29f36d498b8a3d7d0b4d76f5cdf
Niklas Edmundsson <nikke@hpc2n.umu.se>
2026-03-05 13:47:10 +0100

    pertape.pl: Enable JSON formatted default files
    Allow storing and using config files with defaults in a JSON format that
    allows rudimentary comments.
    
    This allows easy creation of various personas of pertape.pl for
    different use cases.

--- util/pertape.pl
+++ util/pertape.pl
@@ -19,54 +19,201 @@
 use warnings;
 use strict;
 use JSON;
+use Cwd qw(abs_path);
 
-# Optinal: Chunk tape reads by this many bytes, usecase for rate limiting reads as to not
-# run out of read tape pool buffer size. Should not be an even multiple of tape size, and
-# this script support splitting into at most 100 chunks.
-my $splitby = 55000000000000;
-# Create commands to issue reads from this pool
-my $sourcepool = "alice_hpc_ku_dk_r06";
-# Create command files in this directory
-my $listdir = "list";
+use Getopt::Long;
+Getopt::Long::Configure (qw(no_ignore_case no_auto_abbrev));
 
-my $tapelist;
+my %opts = (
+	# JSON formatted defaults file, to override the example defaults
+	# for easy multi-usecase scenarios.
+	defaultsfile	=> undef,
+	getdefaults	=> undef,
 
-die "usage pertape.pl sourcehints.json [desthints.json]\n" unless $ARGV[0];
+	# Create commands to issue reads from this pool
+	cmdtemplate	=> '\s %src% rh restore %id%',
+	# %src% identifier default for above
+	cmdsrc		=> 'sourcepool',
+	# Source tape hints file
+	sourcehintfile	=> undef,
+	# Optional destination tape hints file
+	desthintfile	=> undef,
 
-if(open my $tf, '<', $ARGV[0]) {
-  eval {
-   local $SIG{__WARN__} = sub {};
-   local $SIG{__DIE__} = sub {};
+	# Output command files in this directory
+	outdir		=> "list",
+	force		=> 0,
 
-   $tapelist = decode_json(<$tf>);
-  };
-  if($@) {
-   die "Parsing $ARGV[0] as JSON failed: $@";
-  }
-} else { die $!; }
+	# Optional: Chunk tape reads by this many bytes, usecase for rate
+	# limiting reads is to not run out of read tape pool buffer size.
+	# A typical reason for this is when the target write tape pool is
+	# slower than the source read tape pool.
+	# Should not be an even multiple of tape size, and this script support
+	# splitting into at most 100 chunks.
+	splitby		=> 0,
+);
 
-my $done;
 
-if($ARGV[1]){
-  if(open my $tf, '<', $ARGV[1]) {
-    eval {
-     local $SIG{__WARN__} = sub {};
-     local $SIG{__DIE__} = sub {};
-  
-     $done = decode_json(<$tf>);
-    };
-    if($@) {
-     die "Parsing $ARGV[1] as JSON failed: $@";
-    }
-  } else { die $!; }
+my($help);
+
+sub usage()
+{
+	print <<EOH;
+Usage:
+	--defaults, -j		Defaults commented JSON file (optional)
+	--getdefaults, -J	Only show defaults in commented JSON file format
+	--sourcehints, -s	Source hint file (required)
+	--desthints, -d		Destination hint file (optional)
+	--cmdtemplate, -c	Command template (default example: $opts{cmdtemplate})
+	--srcarg, -S		%src% argument for the template (default example: $opts{cmdsrc})
+	--outdir, -D		Output directory (default: $opts{outdir})
+	--force, -f		Force using an existing output directory
+	--splitby, -b		Split lists by this size in bytes (default: $opts{splitby})
+EOH
+	exit 1;
+}
+
+GetOptions (
+		"defaults|j=s" => \$opts{defaultsfile},
+		"getdefaults|J=s" => \$opts{getdefaults},
+		"sourcehints|s=s" => \$opts{sourcehintfile},
+		"desthints|d=s" => \$opts{desthintfile},
+		"cmdtemplate|commandtemplate|c=s" => \$opts{cmdtemplate},
+		"srcarg|cmdsrc|S=s" => \$opts{cmdsrc},
+		"outdir|D=s" => \$opts{outdir},
+		"force|f" => \$opts{force},
+		"splitby|b=i" => \$opts{splitby},
+		"help|h" => \$help,
+	)
+	or die("Failed to parse command line arguments (1)");
+
+if(scalar(@ARGV) > 0) {
+	die("Failed to parse command line arguments (2)");
+}
+
+my $defaults;
+
+# Load the defaults file before showing usage so the correct overridden
+# defaults are shown.
+if($opts{defaultsfile}) {
+	my $df;
+	my $infile;
+
+	foreach my $f ($opts{defaultsfile},"$opts{defaultsfile}.json") {
+		if(open $df, '<', $f) {
+			$infile = $f;
+			last;
+		}
+	}
+	die "Neither defaults file $opts{defaultsfile} nor $opts{defaultsfile}.json found" unless($infile);
+
+	# Read file and remove comments.
+	my @file = grep(!/^\s*#/, (<$df>));
+
+	close($df) or die "Closing $infile: $!";
+
+	eval {
+		local $SIG{__WARN__} = sub {};
+		local $SIG{__DIE__} = sub {};
+
+		# join everything into one line and decode.  decode_json croaks
+		# on error, resulting in program exit on error.
+		$defaults = decode_json(join "", @file);
+	};
+	if($@) {
+		die "Parsing $infile as commented JSON failed: $@";
+	}
+
+	# Apply the defaults. No syntax checking of the values read.
+	foreach my $v (qw(sourcehintfile desthintfile cmdtemplate cmdsrc outdir splitby force))
+	{
+		next unless($defaults->{$v});
+
+		$opts{$v} = $defaults->{$v};
+	}
+}
+
+usage() if($help);
+
+if($opts{getdefaults}) {
+	my $h;
+
+	foreach my $v (qw(sourcehintfile desthintfile cmdtemplate cmdsrc outdir splitby force))
+	{
+		next if($v eq 'cmdsrc' and $opts{cmdtemplate} !~ /%src%/);
+		$h->{$v} = $opts{$v} if($opts{$v});
+	}
+
+	if(-e $opts{getdefaults}) {
+		die "Cowardly refusing to overwrite $opts{getdefaults}";
+	}
+
+	open(my $out, '>', $opts{getdefaults}) or die "open $opts{getdefaults}: $!";
+
+	my $me = abs_path($0);
+	print $out "#! $me -j\n";
+	print $out "# $0 defaults " . scalar(localtime(time())) . "\n";
+	print $out to_json($h, {canonical => 1, pretty => 1}) or die;
+	close $out or die "close $out: $!";
+
+	chmod(0755, $opts{getdefaults}) or die "chmod $opts{getdefaults}: $!";
+
+	print "Wrote $opts{getdefaults} in commented JSON format containing current defaults\nExiting...\n";
+	exit 0;
+}
+
+unless($opts{sourcehintfile}) {
+	warn "sourcehints required";
+	usage();
+}
+
+my $sourcehints;
+
+open my $sh, '<', $opts{sourcehintfile} or die "open $opts{sourcehintfile}: $!";
+eval {
+	local $SIG{__WARN__} = sub {};
+	local $SIG{__DIE__} = sub {};
+
+	$sourcehints = decode_json(<$sh>);
+};
+close($sh) or die "Closing $opts{sourcehintfile}: $!";
+if($@) {
+	die "Parsing $opts{sourcehintfile} as JSON failed: $@";
+}
+
+my $desthints;
+
+if($opts{desthintfile}){
+	open my $dh, '<', $opts{desthintfile} or die "open $opts{desthintfile}: $!";
+	eval {
+		local $SIG{__WARN__} = sub {};
+		local $SIG{__DIE__} = sub {};
+
+		$desthints = decode_json(<$dh>);
+	};
+	close($dh) or die "Closing $opts{desthintfile}: $!";
+	if($@) {
+		die "Parsing $opts{desthintfile} as JSON failed: $@";
+	}
+}
+
+if(-d $opts{outdir}) {
+	die "Directory $opts{outdir} already exists, specify a non-existing directory with -D or override with -f" unless($opts{force});
+}
+else {
+	mkdir $opts{outdir} or die "mkdir $opts{outdir}: $!";
+	print "Created directory $opts{outdir}\n";
 }
 
 my %size;
 
-foreach my $pnfsid (sort { $tapelist->{$a}{order} cmp $tapelist->{$b}{order} }keys %$tapelist) {
-	next if defined $done->{$pnfsid}{volid};
-	$size{$tapelist->{$pnfsid}{volid}}+=$tapelist->{$pnfsid}{size};
-	open(my $fh, ">>", "$listdir/$tapelist->{$pnfsid}{volid}" . sprintf(".%02d",$size{$tapelist->{$pnfsid}{volid}}/$splitby));
-	print $fh  "\\s $sourcepool rh restore $pnfsid\n";
-	close($fh) or die "Close error: $!";
+foreach my $id (sort { $sourcehints->{$a}{order} cmp $sourcehints->{$b}{order} }keys %$sourcehints) {
+	next if defined $desthints->{$id}{volid};
+	$size{$sourcehints->{$id}{volid}}+=$sourcehints->{$id}{size};
+	my $f = "$opts{outdir}/$sourcehints->{$id}{volid}" . sprintf(".%02d",$size{$sourcehints->{$id}{volid}}/$opts{splitby});
+	open(my $fh, ">>", $f) or die "open $f: $!";
+	my $s = $opts{cmdtemplate};
+	$s =~ s/%src%/$opts{cmdsrc}/g;
+	$s =~ s/%id%/$id/g;
+	print $fh  "$s\n";
+	close($fh) or die "Closing $f: $!";
 }
